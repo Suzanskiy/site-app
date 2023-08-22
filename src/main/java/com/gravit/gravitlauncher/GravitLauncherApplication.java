85cf97153a62ac3fef1b879e7482af66becd427b
thereayo <andre7947@gmail.com>
2023-08-22 09:20:14 +0300

    [1]{22.08.23}Add Docker

---
+++ src/main/java/com/gravit/gravitlauncher/GravitLauncherApplication.java
@@ -0,0 +1,13 @@
+package com.gravit.gravitlauncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravitLauncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GravitLauncherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gravit/gravitlauncher/GravitLauncherApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.gravit.gravitlauncher;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class GravitLauncherApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
