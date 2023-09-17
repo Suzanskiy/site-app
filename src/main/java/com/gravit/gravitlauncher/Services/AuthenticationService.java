468e7cdb0977afa779b71735b68926b6ef30cca5
thereayo <andre7947@gmail.com>
2023-09-17 21:47:43 +0300

    csrf protection, authentication. add tables: roles, users_roles.

--- src/main/java/com/gravit/gravitlauncher/Services/AuthenticationService.java
+++ src/main/java/com/gravit/gravitlauncher/Services/AuthenticationService.java
@@ -31,21 +31,6 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
     private final PasswordEncoder passwordEncoder;
 
-//    public AuthenticationResponse register(RegisterRequest request) {
-//        UserDTO user = UserDTO.builder()
-//                .userName(request.getUserName())
-//                .email(request.getEmail())
-//                .password(passwordEncoder.encode(request.getPassword()))
-//                .roleNames(List.of("USER"))
-//                .build();
-//        UserEntity userEntity = userMapper.userToEntity(user);
-//        userRepository.save(userEntity);
-//        return AuthenticationResponse.builder()
-//                .userName(user.getUserName())
-//                .roles(user.getRoleNames())
-//                .message(user.getUserName() + " Registration  successful")
-//                .build();
-//    }
 public void register(RegisterRequest request) {
     UserDTO user = UserDTO.builder()
             .userName(request.getUserName())
@@ -56,32 +41,6 @@ public void register(RegisterRequest request) {
     UserEntity userEntity = userMapper.userToEntity(user);
     userRepository.save(userEntity);
 }
-//    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-//        try {
-//            authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(
-//                            request.getUserName(),
-//                            request.getPassword()
-//                    )
-//            );
-//        } catch (UsernameNotFoundException e) {
-//            throw new RuntimeException("Incorrect username or password");
-//        }
-//
-//        var user = userRepository.findByUserNameIgnoreCase(request.getUserName())
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-//
-//        UserDetails userDetails = userMapper.userEntityToUserDetails(user);
-//
-//        Collection<String> roles = userDetails.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .collect(Collectors.toList());
-//
-//        return AuthenticationResponse.builder()
-//                .userName(userDetails.getUsername())
-//                .roles(roles)
-//                .build();
-//    }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         Authentication authentication = new UsernamePasswordAuthenticationToken(
